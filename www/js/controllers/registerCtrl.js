fe1c0fa67d4cd8026d229ee210b0ba95d96c19f5
ArthurAlban <aateamdev@gmail.com>
2015-04-07 12:22:33 +0200

    Register

--- www/js/controllers/registerCtrl.js
+++ www/js/controllers/registerCtrl.js
@@ -1,7 +1,30 @@
-zonder.controller('registerCtrl', function($scope, $state, $ionicPlatform) {
+zonder.controller('registerCtrl', function($scope, $state, $ionicPlatform, $ionicSlideBoxDelegate) {
+	$scope.userData = {};
+
+	$scope.userData.mail = "";
+	$scope.userData.password = "";
+	$scope.userData.confirmPassword = "";
+	$scope.userData.username = "";
+
+	$scope.displayNextButton = true;
+
+	$scope.disableSwipe = function() {
+		$ionicSlideBoxDelegate.enableSlide(false);
+	};
+	
+	$scope.nextStepRegister = function(){
+		$ionicSlideBoxDelegate.next();
+		$scope.displayNextButton = false;
+	};
+
+	$scope.previousStepRegister = function(){
+		$ionicSlideBoxDelegate.previous();
+		$scope.displayNextButton = true;
+	};
 
 	$scope.login = function() {
-    $state.go("animatedSplashscreen");
-  };
+		$state.go("animatedSplashscreen");
+	};
+
 
 });
