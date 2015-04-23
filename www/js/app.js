b2f8ccedd9d1ff83c89d49d30f71601aed8b9251
alban darrieutort <albandarrieutort@MacBook-Pro-de-alban.local>
2015-04-23 15:08:00 +0200

    create poll option design

--- www/js/app.js
+++ www/js/app.js
@@ -45,5 +45,5 @@ zonder.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
   });
   // if none of the above states are matched, use this as the fallback
 
-  $urlRouterProvider.otherwise('/animatedSplashscreen');
+  $urlRouterProvider.otherwise('/home');
 });
